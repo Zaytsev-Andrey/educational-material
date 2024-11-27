8c16c632bdbbfa86dde74ee524cf49163268c94b
Zaytsev Andrey <zaytsev.andrey.al@yandex.ru>
2024-11-27 21:14:29 +0300

    Initial commit

---
+++ strorage-material/src/main/java/ru/educationalmaterial/storagematerial/EducationalMaterialApplication.java
@@ -0,0 +1,11 @@
+package ru.educationalmaterial.storagematerial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalMaterialApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EducationalMaterialApplication.class, args);
+    }
+}
